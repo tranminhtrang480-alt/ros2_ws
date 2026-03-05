645c59691412bc6ba9a8a1fb3c7fe0e2a3d37b7a
Cody <tranminhtrang480@gmail.com>
2026-03-05 12:17:38 +0800

    Add files via upload

---
+++ build/detect/CMakeFiles/detect_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_node.dir/src/detect_node.cpp.o"
+  "CMakeFiles/detect_node.dir/src/detect_node.cpp.o.d"
+  "detect_node"
+  "detect_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
